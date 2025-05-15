1e02604e775d2275a2e5fd2a923f842b5040715f
sara-babgi <148638368+sara-babgi@users.noreply.github.com>
2025-05-15 18:42:29 +0300

    Update message.js

--- models/message.js
+++ models/message.js
@@ -1,23 +1,23 @@
 const messages = [
   {
     text: "أنت أقوى مما تظن، استمر",
-    image: "/images/m1.png"
+    image: "/image/m1.png"
   },
   {
     text: "كل مهمة تنهيها تقربك من هدفك",
-    image: "/images/m2.png"
+    image: "/image/m2.png"
   },
   {
     text: "النجاح يبدأ بخطوة، خذها الآن",
-    image: "/images/m3.png"
+    image: "/image/m3.png"
   },
   {
     text: "لا تؤجل عمل اليوم إلى الغد",
-    image: "/images/m4.png"
+    image: "/image/m4.png"
   },
   {
     text: "كل إنجاز صغير يُحدث فرقًا كبيرًا",
-    image: "/images/m5.png"
+    image: "/image/m5.png"
   },
 ];
 
